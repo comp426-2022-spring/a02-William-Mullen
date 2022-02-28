db65e72f1ef9f9fb7460700ca2a0c1fd20fb8af1
William Mullen <69743259+William-Mullen@users.noreply.github.com>
2022-02-28 13:36:56 -0500

    guessflip

--- flips.js
+++ flips.js
@@ -2,12 +2,12 @@ import { coinFlips,countFlips } from "./modules/coin.mjs";
 import minimist from "minimist";
 
 var args = minimist(process.argv.slice(2))
-
 var num = args.number;
 
 if (num == undefined) {
     num = 1;
 }
 
-var arr = coinFlips(num);
-console.log(arr);
+var arr = coinFlips(num)
+console.log(arr)
+console.log(countFlips(arr))
--- guess-flip.js
+++ guess-flip.js
@@ -1,2 +1,13 @@
 import { flipACoin } from "./modules/coin.mjs";
-console.log(flipACoin(process.argv.slice(2)))
+import minimist from "minimist";
+
+var args = minimist(process.argv.slice(2))
+
+
+var cal = args.call;
+if ((cal == "heads") | (cal == "tails")) {
+    console.log(flipACoin(cal))
+}
+else {
+    console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]")
+}
